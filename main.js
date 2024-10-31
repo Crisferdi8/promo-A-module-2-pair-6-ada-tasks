724d69b03cbe7f0a09342efeadee92e4d99881b9
Silvia Prieto <info@silviaprieto.com>
2024-10-31 11:38:05 +0100

    server

--- main.js
+++ main.js
@@ -1,29 +1,31 @@
 "use strict";
 
-const tasks = [
-  { name: "Recoger setas en el campo", completed: true, id: 1 },
-  { name: "Comprar pilas", completed: true, id: 2 },
-  { name: "Poner una lavadora de blancos", completed: true, id: 3 },
-  {
-    name: "Aprender cómo se realizan las peticiones al servidor en JavaScript",
-    completed: false,
-    id: 4,
-  },
-];
-console.log(tasks);
+const GITHUB_USER = "sprietoviciana";
+const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
+
+let tasks = [];
 
 const list = document.querySelector(".listTask");
 const checked = document.querySelector(".checked");
 const inputCheck = document.querySelector(".inputCheck");
 
-for (const task of tasks) {
-  list.innerHTML += `
+fetch(SERVER_URL)
+  .then((response) => {
+    return response.json();
+  })
+  .then((data) => {
+    console.log(data);
+    tasks = data.results;
+
+    for (const task of tasks) {
+      list.innerHTML += `
   <li> <input class="inputCheck" id="${task.id}" type="checkbox">
   ${task.name} 
   </li> 
  `;
-  console.log(task);
-}
+      // console.log(task);
+    }
+  });
 
 function handleClick(event) {
   if (event.target.classList.contains("inputCheck")) {
