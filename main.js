274c3e1dfba59c671f26953fc300e1687f4f8105
Silvia Prieto <info@silviaprieto.com>
2024-11-05 11:22:22 +0100

    update

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 
 const GITHUB_USER = "sprietoviciana";
 const SERVER_URL = `https://dev.adalab.es/api/todo/${GITHUB_USER}`;
-
-let tasks = [];
-
 const list = document.querySelector(".listTask");
 const checked = document.querySelector(".checked");
 const inputCheck = document.querySelector(".inputCheck");
 const inputNewTask = document.querySelector(".input");
 const button = document.querySelector(".js-btn-newTask");
+
+//listado vacio
+let tasks = [];
 
 const handleNewTask = (event) => {
   event.preventDefault();
@@ -17,28 +17,20 @@ const handleNewTask = (event) => {
   const addTask = {
     name: `${newTask}`,
     completed: false,
-
-  }
+  };
   tasks.push(addTask);
-  console.log(handleNewTask);
 
   for (const task of tasks) {
     list.innerHTML += `
   <li> <input class="inputCheck" id="${task.id}" type="checkbox">
   ${task.name} 
   </li>
- `
+ `;
   }
-
+  button.addEventListener("click", handleNewTask); //que le pasa al puto boton!!!!
 };
-button.addEventListener("click", handleNewTask);
 
-function handleClick() {
-  inputCheck.classList.add("checked");
-}
-
-
-inputCheck.addEventListener("click", handleClick);
+//llamada al servidor
 fetch(SERVER_URL)
   .then((response) => {
     return response.json();
@@ -53,10 +45,10 @@ fetch(SERVER_URL)
   ${task.name} 
   </li> 
  `;
-      // console.log(task);
     }
   });
 
+// funcion y evento para checkear y tachar
 function handleClick(event) {
   if (event.target.classList.contains("inputCheck")) {
     const listItem = event.target.parentElement;
@@ -65,3 +57,7 @@ function handleClick(event) {
 }
 
 list.addEventListener("click", handleClick);
+
+// inputCheck.addEventListener("click", handleClick);
+
+//coger el valor del input y meterlo a la lista de tareas
